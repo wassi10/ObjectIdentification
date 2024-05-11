c6b38fb61e715470f2ae7643bacdf134359e5f26
wassi10 <khadizawassi@gmail.com>
2024-05-11 20:32:31 +0600

    Deep Learning Added

--- ImageUpload/views.py
+++ ImageUpload/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from ImageUpload.forms import ImageUploadForm
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+
 def handle_uploaded_file(f):
     with open('img.jpg', 'wb+') as destination:
         for chunk in f.chunks():
@@ -9,4 +16,19 @@ def Home(request):
     return render(request, 'index.html')
 
 def Result(request):
+    form = ImageUploadForm(request.POST, request.FILES)
+    if form.is_valid():
+        handle_uploaded_file(request.FILES['image'])
+
+        model = ResNet50(weights='imagenet')
+        img_path = 'img.jpg'
+
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        preds = model.predict(x)
+        print('Predicted',  decode_predictions(preds, top=3)[0])
+
     return render(request, 'result.html')
